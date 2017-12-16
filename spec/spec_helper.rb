2ef65a0397dbf4d9c4c58002c964e355b66e55bf
Eugene Piven <thepegasd@gmail.com>
2017-12-16 23:26:56 +0300

    Removed some garbage from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
 require 'strings2markdown'
-
-RSpec.configure do |config|
-  config.example_status_persistence_file_path = '.rspec_status'
-  config.disable_monkey_patching!
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
