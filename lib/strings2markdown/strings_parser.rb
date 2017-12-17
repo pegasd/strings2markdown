11758c4f4eb11038d994977276c49af2179419b5
Eugene Piven <thepegasd@gmail.com>
2017-12-17 19:58:19 +0300

    Refactored StringsGenerator into a separate file, renamed it too. Crazy.

---
+++ lib/strings2markdown/strings_parser.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'puppet-strings'
+require 'puppet-strings/yard'
+require 'puppet-strings/json'
+
+# Convert puppet-strings output to Markdown format.
+module Strings2markdown
+  # Use puppet-strings to generate a hash.
+  class StringsParser
+    attr_reader :module_resources
+
+    def initialize(module_path)
+      @module_path            = module_path
+      @strings_search_pattern = PuppetStrings::DEFAULT_SEARCH_PATTERNS.map { |path| "#{module_path}/#{path}" }
+
+      PuppetStrings::Yard.setup!
+      args = %w[doc -mmarkdown -n -q --no-progress --no-stats] + @strings_search_pattern
+      YARD::CLI::Yardoc.run(*args)
+
+      @module_resources = {
+        puppet_classes:   YARD::Registry.all(:puppet_class).sort_by!(&:name).map!(&:to_hash),
+        defined_types:    YARD::Registry.all(:puppet_defined_type).sort_by!(&:name).map!(&:to_hash),
+        resource_types:   YARD::Registry.all(:puppet_type).sort_by!(&:name).map!(&:to_hash),
+        providers:        YARD::Registry.all(:puppet_provider).sort_by!(&:name).map!(&:to_hash),
+        puppet_functions: YARD::Registry.all(:puppet_function).sort_by!(&:name).map!(&:to_hash)
+      }
+    end
+
+    def self.parse_module(module_path: '.')
+      # Always clear the YARD registry before each new run
+      YARD::Registry.clear
+
+      yard_hash_generator = Strings2markdown::StringsParser.new(module_path)
+      yard_hash_generator.module_resources
+    end
+  end
+end
--- spec/strings2markdown_spec.rb
+++ spec/strings2markdown_spec.rb
@@ -10,28 +10,4 @@ RSpec.describe Strings2markdown do
   it 'compiles and is a module' do
     expect(Strings2markdown).to be_a(Module)
   end
-
-  it 'gets a hash from puppet-strings' do
-    existing_module_hash = Strings2markdown.parse_module(module_path: './spec/fixtures/test_module')
-
-    expect(existing_module_hash).to be_a(Hash)
-  end
-
-  it 'gets a class and a function from test module' do
-    existing_module_hash = Strings2markdown.parse_module(module_path: './spec/fixtures/test_module')
-    expect(existing_module_hash[:puppet_classes].first[:name]).to eq(:cron)
-    expect(existing_module_hash[:puppet_functions].first[:name]).to eq(:'cron::prep4cron')
-  end
-
-  it 'returns a hash with empty arrays for a non-existant module' do
-    fake_module_hash = Strings2markdown.parse_module(module_path: 'fake_module_name')
-
-    expect(fake_module_hash).to eq(
-      puppet_classes:   [],
-      defined_types:    [],
-      resource_types:   [],
-      providers:        [],
-      puppet_functions: [],
-    )
-  end
 end
