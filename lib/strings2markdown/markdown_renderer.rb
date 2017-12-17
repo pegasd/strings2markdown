4478b1bac47f6ddbacd0395f708a68fb3234b344
Eugene Piven <thepegasd@gmail.com>
2017-12-18 00:59:56 +0300

    Basic template markdown implementation of puppet class

--- lib/strings2markdown/markdown_renderer.rb
+++ lib/strings2markdown/markdown_renderer.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
+require 'erb'
+
 module Strings2markdown
   # Render documentation hash to markdown
   class MarkdownRenderer
     def initialize(module_path)
       @module_resources = Strings2markdown::StringsParser.parse_module(module_path: module_path)
+    end
+
+    def render(path)
+      content = File.read(File.expand_path(path))
+      ERB.new(content, nil, '-').result(binding)
+    end
+
+    def render_classes
+      md_classes = []
+      classes    = @module_resources[:puppet_classes]
+      classes.each do |puppet_class|
+        @puppet_class = puppet_class
+        class_md      = render('lib/templates/resources/puppet_class.erb')
+        md_classes.push(class_md)
+      end
+      md_classes.join("\n")
     end
   end
 end
--- spec/markdown_renderer_spec.rb
+++ spec/markdown_renderer_spec.rb
@@ -3,9 +3,32 @@
 require 'spec_helper'
 
 RSpec.describe Strings2markdown::MarkdownRenderer do
-  md_renderer = described_class.new(module_path: './spec/fixtures/test_module')
+  md_renderer = described_class.new('./spec/fixtures/test_module')
 
   it 'gets initiated properly' do
     expect(md_renderer).to be_an_instance_of(described_class)
+  end
+
+  it 'renders class documentation' do
+    expect(md_renderer.render_classes).to eq(<<~MARKDOWN
+      #### Class: `klass`
+
+      A simple class.
+
+      Use it to do stuff.
+
+      ##### Parameters:
+
+      * `param1`: First param.
+        * Type: `Variant[Integer, Array[Integer, 1]]`
+      * `param2`: Second param.
+        * Type: `Any`
+      * `param3`
+        * Type: `String`
+        * Default value: **'hi'**
+      * `param4`
+        * Type: `Integer`
+    MARKDOWN
+    )
   end
 end
