5ad34432333b09ee701a0d9da44e6039973078d0
Eugene Piven <thepegasd@gmail.com>
2017-12-18 02:12:08 +0300

    RuboCop fix

--- spec/markdown_renderer_spec.rb
+++ spec/markdown_renderer_spec.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-
-RSpec.describe Strings2markdown::MarkdownRenderer do
+pec.describe Strings2markdown::MarkdownRenderer do
   md_renderer = described_class.new('./spec/fixtures/test_module')
-
   it 'gets initiated properly' do
     expect(md_renderer).to be_an_instance_of(described_class)
   end
-
   it 'renders class documentation' do
     expect(md_renderer.render_classes).to eq(
       <<~MARKDOWN
         #### Class: `klass`
-
-        A simple class.
-
-        Use it to do stuff.
-
-        ##### Parameters:
-
-        * `param1`: First param.
+         A simple class.
+         Use it to do stuff.
+         ##### Parameters:
+         * `param1`: First param.
           * Type: `Variant[Integer, Array[Integer, 1]]`
         * `param2`: Second param.
           * Type: `Any`
