1cb7679c13cc57ef0ff7573a7069cc732ede46b6
Eugene Piven <thepegasd@gmail.com>
2017-12-21 22:57:06 +0300

    Implement render_defined_types function

--- lib/strings2markdown/markdown_renderer.rb
+++ lib/strings2markdown/markdown_renderer.rb
@@ -14,15 +14,26 @@ module Strings2markdown
       ERB.new(content, nil, '-').result(binding)
     end
 
-    def render_classes
+    def render_puppet_classes
       md_classes = []
       classes    = @module_resources[:puppet_classes]
       classes.each do |puppet_class|
-        @puppet_class = puppet_class
-        class_md      = render('lib/templates/resources/puppet_class.erb')
-        md_classes.push(class_md)
+        @puppet_class   = puppet_class
+        puppet_class_md = render('lib/templates/resources/puppet_class.erb')
+        md_classes.push(puppet_class_md)
       end
       md_classes.join("\n")
+    end
+
+    def render_defined_types
+      md_defined_types = []
+      defined_types    = @module_resources[:defined_types]
+      defined_types.each do |defined_type|
+        @defined_type   = defined_type
+        defined_type_md = render('lib/templates/resources/defined_type.erb')
+        md_defined_types.push(defined_type_md)
+      end
+      md_defined_types.join("\n")
     end
   end
 end
